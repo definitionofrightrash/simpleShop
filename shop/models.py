69825fb5dc86780a6c3102dcf3147ea734412236
righTrash <definitionofrightrash@gmail.com>
2020-09-21 16:35:12 +0330

    register completed

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,57 @@
-from django.db import models
 
-# Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+'''
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+'''
+
+class Product(models.Model):
+    Code = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=225)
+    QtyInStock = models.IntegerField(default=0)
+    BuyPrice = models.IntegerField()
+    Description = models.CharField(max_length=500, null=True, blank=True)
+    #Image = models.CharField(max_length=200, null=True, blank=True)
+    Image = models.ImageField(upload_to='uploads/%Y/%m/%d/', null=True, blank=True)
+    AddedDate = models.DateTimeField(default=timezone.now())
+    LastEdit = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return str(self.Code) + " " + self.Name + " " + str(self.BuyPrice)
+
+    # def __init__(self, Name, BuyPrice, QtyInStock=0, Description='', Image=None):
+    #     models.Model.__init__(self)
+    #     self.Name = Name
+    #     self.QtyInStock = QtyInStock
+    #     self.BuyPrice = BuyPrice
+    #     self.Description = Description
+    #     if Image:
+    #         self.Image = Image
+    #     self.AddedDate = timezone.now()
+    #     self.LastEdit = timezone.now()
+        
+class Customer(models.Model):
+    Name = models.CharField(max_length=225)
+    Email = models.EmailField(max_length=225, unique=True)
+    Password = models.CharField(max_length=225)
+
+'''
+class Tag(models.Model):
+
+class Category(models.Model):
+
+class Address(models.Model):
+
+class Order(models.Model):
+
+class Order_Product(models.Model):
+'''
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
-
+from .models import Customer
+import hashlib
 def index(request):
-    return render(request,'404.html',{})
+    return render(request,'Eshopper/login.html',{})
+def register(request):
+    username = request.POST['Name']
+    email = request.POST['Email Address']
+    password = request.POST['Password']
+    if(username.strip() == "" or password.strip() == "" or Customer.objects.filter(Email  = email)):
+        return render(request,"Eshopper/login.html")
+    else: 
+        c = Customer(Name = username, Email = email, Password = hashlib.md5(password.encode()))
+        c.save()
+        return render(request,'Eshopper/shop.html',{'Customer':c})
+        
+#def login(request):
+   
